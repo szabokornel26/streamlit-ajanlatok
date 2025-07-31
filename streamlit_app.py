3ae3a4258cd8468b87f96bb5379150884fa2f49a
Szabó Kornél <kornel.szabo2000@gmail.com>
2025-07-31 11:24:16 +0200

    Update streamlit_app.py
    gcp key

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,11 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 # Streamlit Secretsből olvasod be a GCP kulcsot
+gcp_key_json = st.secrets["GCP_SERVICE_ACCOUNT_KEY"]
+gcp_key_dict = json.loads(gcp_key_json)  # így lesz dict
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["GCP_SERVICE_ACCOUNT_KEY"]
-)
-
-client = bigquery.Client(credentials=credentials, project=st.secrets["GCP_SERVICE_ACCOUNT_KEY"]["project_id"])
+credentials = service_account.Credentials.from_service_account_info(gcp_key_dict)
+client = bigquery.Client(credentials=credentials, project=gcp_key_dict["project_id"])
 
 st.set_page_config(layout="wide")
 
